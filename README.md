87d2ab2d2b6cb8a1b31624260c1e208cc78cb6f4
ProsperieEli <ElijahProsperie@gmail.com>
2022-02-15 18:36:42 -0500

    pushing

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # firstPythonApp
 
-A test to create a Python app
+A test to create a Python app.
+
+python -m install django === installs DJANGO dependancies.
+
+pythen -m venv {file name and path} === creates your venv folder.
+
+# Type convertions
+
+int() = convert to integer
+str() = convert to string
+bool() = convert to boolean
+float() = convert to decimal
---
+++ app.py
@@ -0,0 +1,50 @@
+#declare variable and types in Python
+
+
+# age = 10
+# price = 19.98
+# first_name = 'Elijah'
+# is_online = True
+# print(age, price, first_name, is_online)
+
+
+# #inputs
+# #concating inputs. The space is required and you would answer in the terminal.
+# greeting = input('How are you doing? ')
+# # print('Good ' + greeting)
+
+# #type convertion
+# birth_Year = input("Enter your birth year: ")
+# age = 2022 - int(birth_Year)
+# # print(age)
+
+#Calculator exercise
+# value1 = input("First: ")
+# value2 = input("Second: ")
+# total = float(value1) + float(value2)
+# print(total)
+
+#strings
+# course = 'Python for beginners'
+# print(course.upper())
+# print(course.lower())
+# #finds the input at the index. Starts at 0
+# print(course.find('y'))
+# #search if a string is in a variable
+# print('Python' in course)
+# print(course.replace('beginners', 'seniors'))
+# print(course)
+
+#Math stuff
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+#double // returns number as an integer where one / returns floating
+print(10 // 3)
+print(10 % 3)
+#exponent
+print(10 ** 3)
+
+x = 10
+x = x + 3
